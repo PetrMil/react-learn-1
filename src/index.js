6276e577547f791ced1b4cd2ac04f59dff8a38b6
Sergey Vassilyev <commandovmk@gmail.com>
2017-10-16 23:39:37 +0600

    my func and attributes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
 import Game from './board-game.js'
 
-import Test1 from './test1.js'
+import {App} from './test1.js'
 
 // ReactDOM.render(
 //   <Game />,
 //   document.getElementById('root')
 // );
 
+var myFunc = function(param) {
+  console.log(param)
+}
+
 ReactDOM.render(
-  <Test1 />,
+  <div>
+    <App capital={true} count={5} myfunc={myFunc} onClick={myFunc} />
+    <App capital={false} count={2} myfunc={myFunc} />
+    <App capital={true} count={7} myfunc={myFunc} />
+  </div>,
+
   document.getElementById('root')
 );
 
--- src/test1.js
+++ src/test1.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component {
+export class App extends React.Component {
   render() {
+
+
     const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+
     
     const navLinks = [] 
 
@@ -13,18 +16,22 @@ class App extends React.Component {
       const a = (
         <div> 
           <a href={'/' + page}> 
-            {page} 
+            {this.props.capital ? page.toUpperCase() : page} 
           </a>
         </div>
       ) 
+
+      if (navLinks.length > this.props.count) return;
 
       navLinks.push(a);
 
     });
 
+    this.props.myfunc(navLinks)
     return navLinks
   }
 }
 
+export var counts = 10
 
-export default App
+
